6f3344b9f9fa251adf1d1b95078bf4035ca72e02
Leslie Tom <leslie.tom@gmail.com>
2012-06-25 16:27:26 -0700

    Got to run 'index.html'!
    From watching YouTube calicoJake Tutorial 2.
    http://www.youtube.com/watch?v=Wb-K40mkhEU&feature=plcp

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
+#
+#@app.route("/")
+#def hello():
+#    return "Hello World!"
+###Leslie needs to route this to the index.html page.
+#
+#
+#if __name__ == "__main__":
+#    app.debug = True
+#    app.run()
+#
+##This comes from YouTube tutorial.
+import flask, flask.views
+app = flask.Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+#class View(flask.views.MethodView):
+#    def get(self):
+#        return 'Hello There!'
 
-if __name__ == "__main__":
-    app.run()
+class View(flask.views.MethodView):
+    def get(self):
+        return flask.render_template('index.html')
+    
+app.add_url_rule('/', view_func=View.as_view('Main'), methods=['GET'])
+
+app.debug = True
+app.run()
